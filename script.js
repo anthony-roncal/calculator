1ae934faaa2c5c15bd7e14883a1ff3d067da0700
Anthony <acr1191@gmail.com>
2023-01-06 15:40:18 -0800

    add ability to update the display when number buttons are clicked

--- script.js
+++ script.js
@@ -18,4 +18,20 @@ function operate(operator, a, b) {
     return operator(a, b);
 }
 
-console.log(operate(divide, 10, 4));
+let displayValue = 0;
+const display = document.querySelector('#display-value');
+
+const numberButtons = document.querySelectorAll('#numbers > button');
+numberButtons.forEach(number => number.addEventListener("click", updateDisplay));
+
+function updateDisplay(e) {
+    if (display.textContent === "0" && e.target.textContent === "0") {
+        display.textContent = "0";
+    } else if (display.textContent === "0" && e.target.textContent !== "0") {
+        display.textContent = e.target.textContent;
+    }
+    else {
+        display.textContent += e.target.textContent;
+    }
+    displayValue = Number(display.textContent);
+}
